d8c049bffa5156dd8ee4d5a7d2ee7cf2d7ef5463
Bit Player <shikanchen@users.noreply.github.com>
2022-05-02 16:15:55 -0700

    s

--- server.js
+++ server.js
@@ -4,16 +4,67 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
+    type Query {
+        course(id: Int!): Course
+        courses(topic: String): [Course]
+    },
+    type Course {
+        id: Int
+        title: String
+        author: String
+        description: String
+        topic: String
+        url: String
+    }
 `);
 
-// The root provides a resolver function for each API endpoint
+var coursesData = [
+    {
+        id: 1,
+        title: 'The Complete Node.js Developer Course',
+        author: 'Andrew Mead, Rob Percival',
+        description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        topic: 'Node.js',
+        url: 'https://codingthesmartway.com/courses/nodejs/'
+    },
+    {
+        id: 2,
+        title: 'Node.js, Express & MongoDB Dev to Deployment',
+        author: 'Brad Traversy',
+        description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        topic: 'Node.js',
+        url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
+    },
+    {
+        id: 3,
+        title: 'JavaScript: Understanding The Weird Parts',
+        author: 'Anthony Alicea',
+        description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        topic: 'JavaScript',
+        url: 'https://codingthesmartway.com/courses/understand-javascript/'
+    }
+]
+var getCourse = function(args) { 
+    var id = args.id;
+    return coursesData.filter(course => {
+        return course.id == id;
+    })[0];
+}
+var getCourses = function(args) {
+    if (args.topic) {
+        var topic = args.topic;
+        return coursesData.filter(course => course.topic === topic);
+    } else {
+        return coursesData;
+    }
+}
 var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
+    course: () => {
+        return getCourse;
+    },
+    courses: () => {
+        return getCourses;
+    }
 };
 
 var app = express();
--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-//import { createApp } from 'vue'
-//import App from './App.vue'
 import { createApp } from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
