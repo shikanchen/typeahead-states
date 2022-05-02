bb23055f415c916f5896f4a7d3d3dde843ea3657
Bit Player <shikanchen@users.noreply.github.com>
2022-05-02 15:41:37 -0700

    front render

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,7 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <img alt="typedahead logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,53 @@
+//import { createApp } from 'vue'
+//import App from './App.vue'
 import { createApp } from 'vue'
-import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.component('enz-typeahead', {
+	props: ['states', 'value'], // <-- prop value is added
+	mounted: function () {
+		var me = this;
+		
+		var states = new Bloodhound({
+			datumTokenizer: Bloodhound.tokenizers.whitespace,
+			queryTokenizer: Bloodhound.tokenizers.whitespace,
+			local: this.states // <-- states is being attached here
+		});
+		
+		jQuery(me.$refs.thead_001).typeahead({
+			hint: true,
+			highlight: true,
+			minLength: 1
+		}, {
+			name: 'states',
+			source: states
+		});
+		
+		// listen to 'typeahead:select' and emit the input event
+		jQuery(me.$refs.thead_001).bind('typeahead:select', function (e) {
+			me.$emit('input', e.target.value);
+		});
+		
+		// listen to 'typeahead:autocomplete' and emit the input event
+		jQuery(me.$refs.thead_001).bind('typeahead:autocomplete', function (e) {
+			me.$emit('input', e.target.value);
+		});
+		
+	},
+	template: '<input class="typeahead" type="text" ref="thead_001" placeholder="States of USA">'
+});
+
+new Vue({
+	el: "#app",
+	data: {
+		selectedState: null, // <-- Variable with default value
+		ourStates: ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District ", "of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+		
+	}
+});
