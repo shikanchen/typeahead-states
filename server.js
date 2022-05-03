a3282f15baf056c9949523a084718ed21aabc147
Bit Player <shikanchen@users.noreply.github.com>
2022-05-02 19:19:43 -0700

    typeahead autocomplete

--- server.js
+++ server.js
@@ -1,73 +1,52 @@
 var express = require('express');
+var cors = require('cors');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
-// Construct a schema, using GraphQL schema language
+// GraphQL schema
 var schema = buildSchema(`
-    type Query {
-        course(id: Int!): Course
-        courses(topic: String): [Course]
-    },
-    type Course {
-        id: Int
-        title: String
-        author: String
-        description: String
-        topic: String
-        url: String
-    }
+  type Query {
+    states(keyword: String): [State]
+  },
+  type State {
+    id: Int
+    name: String
+  }
 `);
 
-var coursesData = [
-    {
-        id: 1,
-        title: 'The Complete Node.js Developer Course',
-        author: 'Andrew Mead, Rob Percival',
-        description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs/'
-    },
-    {
-        id: 2,
-        title: 'Node.js, Express & MongoDB Dev to Deployment',
-        author: 'Brad Traversy',
-        description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
-    },
-    {
-        id: 3,
-        title: 'JavaScript: Understanding The Weird Parts',
-        author: 'Anthony Alicea',
-        description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
-        topic: 'JavaScript',
-        url: 'https://codingthesmartway.com/courses/understand-javascript/'
-    }
+var statesData = [
+  {
+    id: 0,
+    name: 'New York'
+  },
+  {
+    id: 1,
+    name: 'California'
+  }
 ]
-var getCourse = function(args) { 
-    var id = args.id;
-    return coursesData.filter(course => {
-        return course.id == id;
-    })[0];
+
+var getStates = function(args) { 
+  if (args.keyword) {
+    var keyword = args.keyword;
+    return statesData.filter(state => state.name.includes(keyword));
+  } else {
+    return statesData;
+  }
 }
-var getCourses = function(args) {
-    if (args.topic) {
-        var topic = args.topic;
-        return coursesData.filter(course => course.topic === topic);
-    } else {
-        return coursesData;
-    }
-}
+
+// Root resolver
 var root = {
-    course: () => {
-        return getCourse;
-    },
-    courses: () => {
-        return getCourses;
-    }
+  states: getStates,
 };
 
 var app = express();
+
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
--- src/main.js
+++ src/main.js
@@ -7,45 +7,31 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
-Vue.component('enz-typeahead', {
-	props: ['states', 'value'], // <-- prop value is added
-	mounted: function () {
-		var me = this;
-		
-		var states = new Bloodhound({
-			datumTokenizer: Bloodhound.tokenizers.whitespace,
-			queryTokenizer: Bloodhound.tokenizers.whitespace,
-			local: this.states // <-- states is being attached here
-		});
-		
-		jQuery(me.$refs.thead_001).typeahead({
-			hint: true,
-			highlight: true,
-			minLength: 1
-		}, {
-			name: 'states',
-			source: states
-		});
-		
-		// listen to 'typeahead:select' and emit the input event
-		jQuery(me.$refs.thead_001).bind('typeahead:select', function (e) {
-			me.$emit('input', e.target.value);
-		});
-		
-		// listen to 'typeahead:autocomplete' and emit the input event
-		jQuery(me.$refs.thead_001).bind('typeahead:autocomplete', function (e) {
-			me.$emit('input', e.target.value);
-		});
-		
-	},
-	template: '<input class="typeahead" type="text" ref="thead_001" placeholder="States of USA">'
-});
-
 new Vue({
-	el: "#app",
-	data: {
-		selectedState: null, // <-- Variable with default value
-		ourStates: ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District ", "of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
-		
+  el: "#app",
+  data: {
+    keyword: null,
+		states: []
+  },
+  methods: {
+    queryForStates(event) {
+			const keyword = event.target.value
+			this.keyword = keyword
+			var self = this
+			$.get("http://localhost:4000/graphql",
+				{
+					query : "query getFilteredStates($keyword: String) {states(keyword: $keyword) {name}}",
+					operationName : "getFilteredStates",
+					variables : "{\"keyword\": \""+self.keyword+"\"}"
+				},
+				function(data) {
+					const filteredStates = data.data.states
+					self.states = filteredStates
+				}
+			);
+    },
+		completeState(name) {
+			this.keyword = name
+		}
 	}
 });
